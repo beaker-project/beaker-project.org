c4d4df9854cd48f381765c038b7ccdfef41d515d
Dan Callaghan <dcallagh@redhat.com>
2013-02-27 11:44:32 +1000

    show a warning on docs built from branches other than master

--- docs/conf.py
+++ docs/conf.py
@@ -1,4 +1,6 @@
 
+# vim: set fileencoding=utf-8 :
+
 # The real conf.py lives in Beaker's source, so we load it from there and then
 # override some template settings below.
 import os, os.path
@@ -13,3 +15,18 @@ intersphinx_mapping['python'] = ('http://docs.python.org/',
         os.path.join(os.path.dirname(__file__), '..', 'python-intersphinx.inv'))
 intersphinx_mapping['beakerdev'] = ('../dev/',
         os.path.join(os.path.dirname(__file__), '..', 'dev', 'objects.inv'))
+
+html_context = {}
+if os.path.basename(os.environ['BEAKER']) == 'master':
+    html_context['branch_warning'] = ''
+elif os.path.basename(os.environ['BEAKER']) == 'develop':
+    html_context['branch_warning'] = u"""
+        You are viewing the development version of Beaker’s documentation. It 
+        is not final and may be changed before the next release.
+        """
+else:
+    html_context['branch_warning'] = u"""
+        You are viewing a branched version of Beaker’s documentation. The 
+        latest released version of the documentation contains more 
+        up-to-date information.
+        """
