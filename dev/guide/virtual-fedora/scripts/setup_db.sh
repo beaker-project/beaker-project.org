4b52462289398bc6a9da91911f1d4d272b5659ef
Amit Saha <asaha@redhat.com>
2013-07-03 13:44:43 +1000

    Documentation for setting up a virtual Beaker test bed.
    Bug: 979211
    Change-Id: Ic374990dd3757d1d533ef0a33e10687bff487cf9
    
    Former-commit-id: 71edb8134050749dad113863ec2a41f9c56bd0f7

---
+++ dev/guide/virtual-fedora/scripts/setup_db.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+# This script will setup MariaDB, create the beaker DB
+# and populate it's tables
+# Create an admin user for Beaker
+
+set -e
+
+# root
+if (( EUID != 0 )); then
+   echo "You must be root to do this." 1>&2
+   exit 100
+fi
+
+#Let's apply some useful settings to the MariaDB server.
+cp /etc/my.cnf /etc/my.cnf-orig
+cat /etc/my.cnf-orig | awk '
+        {print $0};
+        /\[mysqld\]/ {
+            print "character-set-server=utf8";
+        }' > /etc/my.cnf
+
+systemctl start mysqld
+systemctl enable mysqld
+
+# setup Beaker DB
+echo "CREATE DATABASE beaker;" | mysql
+echo "GRANT ALL ON beaker.* TO 'beaker'@'localhost' IDENTIFIED BY 'beaker';" | mysql
+
+# initialize Beaker DB tables
+echo -n "Please enter a login name for the Beaker admin account: "
+read admin
+echo -n "Please enter an email address for $admin: "
+read email
+password="dummy"
+stty -echo
+trap "stty echo; exit" INT TERM EXIT
+while [ "$password" != "$password_again" ]; do
+    echo -n "Please enter a passowrd for $admin: "
+    read password
+    echo
+    echo -n "Please enter the password again: "
+    read password_again
+    echo
+    if [ "$password" != "$password_again" ]; then
+        echo "Passwords don't match, try again"
+        echo
+    fi
+done
+stty echo
+trap - INT TERM EXIT
+su -s /bin/sh apache -c "beaker-init -u \"$admin\" -p \"$password\" -e \"$email\""
