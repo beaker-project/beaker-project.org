999ea81dc442d03230c0103e88c38e49b58576f2
Martin Styk <mastyk@redhat.com>
2020-02-21 16:22:49 +0100

    python3 build repos script
    Signed-off-by: Martin Styk <mastyk@redhat.com>

--- build-yum-repos.py
+++ build-yum-repos.py
@@ -1,29 +1,27 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 
 """
 Script for generating yum repos for Beaker
 
-This script will pull down the latest Beaker and related packages from 
-Brew/Koji, and spit out a hierarchy of directories containing the 
+This script will pull down the latest Beaker and related packages from
+Brew/Koji, and spit out a hierarchy of directories containing the
 various yum repos for Beaker.
 """
 
-import sys
-import os, os.path
 import glob
-import errno
+import os
 import re
-import shutil
-import glob
-import urlparse
-import requests
-import time
+import subprocess
+import sys
 import tempfile
-import rfc822
-from ConfigParser import SafeConfigParser
+import time
+from configparser import ConfigParser
+from email.utils import parsedate
+from shutil import which
+from xmlrpc.client import Fault
+
 import koji
-import xmlrpclib
-import createrepo # needs createrepo >= 0.9
+import requests
 
 # This is the GPG key id of the redhatengsystems signing key,
 # as it is known to Koji (8 characters lower case).
@@ -33,34 +31,37 @@ EPEL_GPG_KEY_ID = '352c64e5'
 
 # We need the hub and package URLs for Koji and Brew.
 # We can load those from the relevant config files.
-koji_config = SafeConfigParser()
+koji_config = ConfigParser()
 koji_config.read(['/etc/brewkoji.conf', '/etc/koji.conf'] +
-        glob.glob('/etc/koji.conf.d/*.conf') +
-        [os.path.expanduser('~/.koji/config')])
+                 glob.glob('/etc/koji.conf.d/*.conf') +
+                 [os.path.expanduser('~/.koji/config')])
+
 
 def ensure_dir(path):
     if not os.path.exists(path):
         os.makedirs(path)
     assert os.path.isdir(path), '%s must be a directory' % path
 
+
 def fetch(url, dest):
     response = requests.get(url, stream=True)
     response.raise_for_status()
     dirname, basename = os.path.split(dest)
     fd, temp_path = tempfile.mkstemp(prefix='.' + basename, dir=dirname)
-    f = os.fdopen(fd, 'w')
+    f = os.fdopen(fd, 'wb')
     try:
         for chunk in response.iter_content(chunk_size=4096):
             if chunk:
                 f.write(chunk)
         f.flush()
-        mtime = time.mktime(rfc822.parsedate(response.headers['Last-Modified']))
+        mtime = time.mktime(parsedate(response.headers['Last-Modified']))
         os.utime(temp_path, (mtime, mtime))
-        os.chmod(temp_path, 0644)
+        os.chmod(temp_path, 0o644)
         os.rename(temp_path, dest)
     except:
         os.unlink(temp_path)
         raise
+
 
 class TargetRepo(object):
     """
@@ -69,7 +70,8 @@ class TargetRepo(object):
     then call build() to do it.
     """
 
-    def __init__(self, name, distro, tag, arches, downgradeable, all_packages, signed, koji_profile):
+    def __init__(self, name, distro, tag, arches, downgradeable, all_packages, signed,
+                 koji_profile):
         self.name = name
         self.distro = distro
         self.tag = tag
@@ -86,6 +88,9 @@ class TargetRepo(object):
 
     def __repr__(self):
         return '<%s %r>' % (self.__class__.__name__, self.output_dir)
+
+    def __lt__(self, other):
+        return self.name < other.name
 
     @property
     def output_dir(self):
@@ -106,8 +111,8 @@ class TargetRepo(object):
         self.basedir = dest
         if os.path.islink(self.output_dir):
             raise RuntimeError(
-                    'Attempted to build yum repo into symlink %s\n'
-                    'Hint: delete the symlink first' % self.output_dir)
+                'Attempted to build yum repo into symlink %s\n'
+                'Hint: delete the symlink first' % self.output_dir)
         ensure_dir(self.output_dir)
         if self.all_packages:
             self._mirror_all_rpms()
@@ -117,8 +122,8 @@ class TargetRepo(object):
         self._clean_unused_rpms()
 
     def _mirror_all_rpms(self):
-        print 'Mirroring RPMs for %r: all packages in %s tagged %s' % (
-                self, self.koji_profile, self.tag)
+        print('Mirroring RPMs for %r: all packages in %s tagged %s' % (
+            self, self.koji_profile, self.tag))
         hub_url = koji_config.get(self.koji_profile, 'server')
         koji_session = koji.ClientSession(hub_url)
         if self.downgradeable:
@@ -129,11 +134,11 @@ class TargetRepo(object):
             raise ValueError('No packages in tag %s' % self.tag)
         rpms, builds = result
         self.rpm_names.update(rpm['name'] for rpm in rpms
-                if rpm['name'] not in self.excluded_rpms)
+                              if rpm['name'] not in self.excluded_rpms)
         self._mirror_rpms_for_build(self.koji_profile, builds, rpms)
 
     def _mirror_rpms(self, koji_profile, package_names):
-        print 'Mirroring RPMs for %r: listed packages in %s' % (self, koji_profile)
+        print('Mirroring RPMs for %r: listed packages in %s' % (self, koji_profile))
         hub_url = koji_config.get(koji_profile, 'server')
         koji_session = koji.ClientSession(hub_url)
         koji_session.multicall = True
@@ -142,19 +147,22 @@ class TargetRepo(object):
             # As a workaround we only grab the latest tagged build from EPEL.
             if self.downgradeable and not koji_profile == 'koji':
                 koji_session.listTaggedRPMS(
-                        self.package_tags.get(package_name, self.tag),
-                        inherit=True, package=package_name)
+                    self.package_tags.get(package_name, self.tag),
+                    inherit=True, package=package_name)
             else:
                 koji_session.getLatestRPMS(
-                        self.package_tags.get(package_name, self.tag),
-                        package_name)
+                    self.package_tags.get(package_name, self.tag),
+                    package_name)
         results = koji_session.multiCall()
         for i, result in enumerate(results):
             if 'faultCode' in result:
-                raise xmlrpclib.Fault(result['faultCode'], result['faultString'])
+                raise Fault(result['faultCode'], result['faultString'])
             if not result or not result[0] or not result[0][1]:
-                raise ValueError('Package %s not in tag %s in %s' % (package_names[i],
-                        self.package_tags.get(package_names[i], self.tag), koji_profile))
+                raise ValueError('Package %s not in tag %s in %s' % (
+                    package_names[i],
+                    self.package_tags.get(package_names[i], self.tag),
+                    koji_profile
+                ))
             (rpms, builds), = result
             self._mirror_rpms_for_build(koji_profile, builds, rpms)
 
@@ -168,35 +176,35 @@ class TargetRepo(object):
             if rpm['name'] not in self.rpm_names and rpm['arch'] != 'src':
                 continue
             filename = os.path.join(self.output_dir,
-                    os.path.basename(pathinfo.rpm(rpm)))
+                                    os.path.basename(pathinfo.rpm(rpm)))
             if self.signed:
                 key_id = GPG_KEY_ID
                 if koji_profile == 'koji':
                     key_id = EPEL_GPG_KEY_ID
-                # For signed RPMs, the actual file we want to download is 
-                # always *bigger* than the size indicated in Koji. Koji only 
+                # For signed RPMs, the actual file we want to download is
+                # always *bigger* than the size indicated in Koji. Koji only
                 # tracks the original size of the RPM before signing.
                 if os.path.exists(filename) and os.path.getsize(filename) > rpm['size']:
-                    print 'Skipping %s' % filename
+                    print('Skipping %s' % filename)
                 else:
                     url = os.path.join(pathinfo.build(builds[rpm['build_id']]),
-                            pathinfo.signed(rpm, key_id))
-                    print 'Fetching %s' % url
+                                       pathinfo.signed(rpm, key_id))
+                    print('Fetching %s' % url)
                     fetch(url, filename)
             else:
                 # For unsigned RPMs we can check the exact size here.
                 if os.path.exists(filename) and os.path.getsize(filename) == rpm['size']:
-                    print 'Skipping %s' % filename
+                    print('Skipping %s' % filename)
                 else:
                     url = os.path.join(pathinfo.build(builds[rpm['build_id']]),
-                            pathinfo.rpm(rpm))
-                    print 'Fetching %s' % url
+                                       pathinfo.rpm(rpm))
+                    print('Fetching %s' % url)
                     fetch(url, filename)
             self.rpm_filenames.add(os.path.basename(filename))
 
     def _mirror_rpms_for_task(self, koji_session, task_id, filenames):
         patterns = [re.compile(r'%s-\d.*\.(\w+)\.rpm' % re.escape(r))
-                for r in self.rpm_names]
+                    for r in self.rpm_names]
         for filename in filenames:
             for pattern in patterns:
                 m = pattern.match(filename)
@@ -205,9 +213,9 @@ class TargetRepo(object):
                     if arch in self.arches + ['noarch', 'src']:
                         dest_filename = os.path.join(self.output_dir, filename)
                         if os.path.exists(dest_filename):
-                            print 'Skipping %s' % dest_filename
+                            print('Skipping %s' % dest_filename)
                         else:
-                            print 'Fetching %s from task id %s' % (filename, task_id)
+                            print('Fetching %s from task id %s' % (filename, task_id))
                             contents = koji_session.downloadTaskOutput(task_id, filename)
                             dest_file = open(dest_filename, 'w')
                             dest_file.write(contents)
@@ -215,36 +223,36 @@ class TargetRepo(object):
                         self.rpm_filenames.add(filename)
 
     def _create_repo_metadata(self):
-        print 'Creating repodata for %r' % self
+        print('Creating repodata for %r' % self)
         ensure_dir(self.output_dir)
-        conf = createrepo.MetaDataConfig()
-        conf.database = False
-        conf.outputdir = self.output_dir
-        conf.directory = self.output_dir
-        conf.directories = [self.output_dir]
-        if re.match(r'RedHatEnterpriseLinux[345]$', self.distro):
-            conf.sumtype = 'sha'
-        conf.pkglist = self.rpm_filenames
-        # not sure what this is, but it defaults to True in newer createrepo,
-        # but it makes yum explode
-        conf.collapse_glibc_requires = False
-        mdgen = createrepo.MetaDataGenerator(config_obj=conf)
-        mdgen.doPkgMetadata()
-        mdgen.doRepoMetadata()
-        mdgen.doFinalMove()
+        checksum_type = 'sha' if 'RedHatEnterpriseLinux5' in self.distro else 'sha256'
+
+        # Using tmp file for pkglist option
+        # includepkg option accept one pkg
+        with tempfile.NamedTemporaryFile() as pkglist:
+            pkglist.write('\n'.join(self.rpm_filenames).encode())
+            subprocess.run(['createrepo_c',
+                            '--no-database',  # Don't create SQLite DB in repo
+                            '--checksum', checksum_type,  # repomd.xml checksum
+                            '--pkglist', pkglist.name,  # List of included RPMs
+                            '--outputdir', self.output_dir,  # Output dir
+                            '--quiet',  # Run quietly
+                            self.output_dir
+                            ])
 
     def _clean_unused_rpms(self):
         for filename in glob.iglob(os.path.join(self.output_dir, '*.rpm')):
             if os.path.basename(filename) not in self.rpm_filenames:
-                print 'Removing %s' % filename
+                print('Removing %s' % filename)
                 os.unlink(filename)
 
+
 def target_repos_from_config(*config_filenames):
-    config = SafeConfigParser()
-    config.optionxform = str # package names are case sensitive
+    config = ConfigParser()
+    config.optionxform = str  # package names are case sensitive
     for filename in config_filenames:
-        print 'reading %s' % filename
-        config.readfp(open(filename, 'r'))
+        print('Reading %s' % filename)
+        config.read_file(open(filename, 'r'), filename)
 
     repos = {}
     testing_repos = {}
@@ -263,21 +271,22 @@ def target_repos_from_config(*config_filenames):
             if config.has_option(section, 'signed'):
                 signed = config.getboolean(section, 'signed')
             repos[descr] = TargetRepo(name=config.get(section, 'name'),
-                    distro=config.get(section, 'distro'),
-                    arches=config.get(section, 'arches').split(),
-                    tag=config.get(section, 'tag'),
-                    downgradeable=downgradeable,
-                    all_packages=all_packages,
-                    signed=signed,
-                    koji_profile=koji_profile)
+                                      distro=config.get(section, 'distro'),
+                                      arches=config.get(section, 'arches').split(),
+                                      tag=config.get(section, 'tag'),
+                                      downgradeable=downgradeable,
+                                      all_packages=all_packages,
+                                      signed=signed,
+                                      koji_profile=koji_profile)
             testing_repos[descr] = TargetRepo(name=config.get(section, 'testing-name'),
-                    distro=config.get(section, 'distro'),
-                    arches=config.get(section, 'arches').split(),
-                    tag=config.get(section, 'testing-tag'),
-                    downgradeable=downgradeable,
-                    all_packages=all_packages,
-                    signed=False, # testing repos are always unsigned for now
-                    koji_profile=koji_profile)
+                                              distro=config.get(section, 'distro'),
+                                              arches=config.get(section, 'arches').split(),
+                                              tag=config.get(section, 'testing-tag'),
+                                              downgradeable=downgradeable,
+                                              all_packages=all_packages,
+                                              signed=False,
+                                              # testing repos are always unsigned for now
+                                              koji_profile=koji_profile)
             if config.has_option(section, 'skip') and config.getboolean(section, 'skip'):
                 skipped.add(descr)
             if config.has_option(section, 'excluded-rpms'):
@@ -292,36 +301,43 @@ def target_repos_from_config(*config_filenames):
             tag, _, source = rest[len('packages.'):].partition('.')
             for package_name, rpm_names in config.items(section):
                 repos[descr].add_package(package_name, rpm_names.split(),
-                        tag=tag, koji_profile=source or None)
+                                         tag=tag, koji_profile=source or None)
                 # appending -testing here is perhaps an unwise hack...
                 # we just need to work towards eliminating these exceptional sections
                 testing_tag = tag + '-testing' if not tag.endswith('-testing') else tag
                 testing_repos[descr].add_package(package_name, rpm_names.split(),
-                        tag=testing_tag, koji_profile=source or None)
+                                                 tag=testing_tag, koji_profile=source or None)
         else:
             raise ValueError('Unrecognised section: %s' % rest)
     for descr in skipped:
         del repos[descr]
         del testing_repos[descr]
-    return sorted(repos.values() + testing_repos.values())
+    return sorted(list(repos.values()) + list(testing_repos.values()))
+
 
 if __name__ == '__main__':
     from optparse import OptionParser
+
     parser = OptionParser('usage: %prog [options]',
-            description='Builds a hierarchy of Beaker repos according to a config file.')
+                          description='Builds a hierarchy of Beaker repos '
+                                      'according to a config file.')
     parser.add_option('-d', '--dest', metavar='DIR',
-            help='build repos under DIR [default: %default]')
+                      help='build repos under DIR [default: %default]')
     parser.add_option('-c', '--config', metavar='FILE', action='append', dest='config_filenames',
-            help='load repo configuration from FILE')
+                      help='load repo configuration from FILE')
     parser.add_option('--repo', metavar='REPO', action='append', dest='repos',
-            help='only build REPO [default: all repos]')
+                      help='only build REPO [default: all repos]')
     parser.add_option('--distro', metavar='DISTRO', action='append', dest='distros',
-            help='only build repos for DISTRO [default: all distros]')
+                      help='only build repos for DISTRO [default: all distros]')
     parser.set_defaults(dest='./yum')
 
     options, args = parser.parse_args()
     if args:
         parser.error('This program does not accept positional args')
+    if not which('createrepo_c'):
+        print('Missing createrepo_c. Please install createrepo_c before using build-yum-repos',
+              file=sys.stderr)
+        sys.exit(1)
 
     target_repos = target_repos_from_config(*options.config_filenames or ['yum-repos.conf'])
 
