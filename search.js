6b277370ad949d9b91d6af609511b620b82e300b
Renan Rodrigo <rebarbos@redhat.com>
2020-06-04 10:57:41 +0200

    chore: refactor inline css and javascript
    Move inline style and script elements to files
    
    Fix #22
    
    Signed-off-by: Renan Rodrigo <rebarbos@redhat.com>

---
+++ search.js
@@ -0,0 +1,9 @@
+(function() {
+    var cx = '000379057217241479047:jjwkmhjefoc';
+    var gcse = document.createElement('script');
+    gcse.type = 'text/javascript';
+    gcse.async = true;
+    gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(gcse, s);
+})();
